fa428b85efffeebe322b3eff20770bc9cdcd00ca
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-12-03 17:06:38 +0100

    refactor: refactor boilerplate code

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
-
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', (_, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', (_, res) => {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
