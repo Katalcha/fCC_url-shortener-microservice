a7c7010a1d5cf65d42ba4f2cd69e8c094d99751a
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-12-03 17:52:07 +0100

    feat: add mongoose schema and model

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
+
+const clientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+try {
+  mongoose.connect(`${process.env.MONGO_PROTOCOL}${process.env.MONGO_USER}${process.env.MONGO_PW}${process.env.MONGO_URI}`, clientOptions);
+} catch (err) {
+  console.log(err);
+} finally {
+  mongoose.disconnect();
+}
+
+const urlSchema = new mongoose.Schema({
+  original_url: { type: String, required: true },
+  short_url: Number
+});
+
+const URL = mongoose.model('URL', urlSchema);
+
+
 const app = express();
 
 app.use(cors());
@@ -12,6 +31,14 @@ app.get('/', (_, res) => {
 
 app.get('/api/hello', (_, res) => {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('api/shorturl', (req, res) => {
+  res.json({ original_url: "", short_url: "" });
+});
+
+app.get('api/shorturl/:short-url', (req, res) => {
+
 });
 
 const port = process.env.PORT || 3000;
